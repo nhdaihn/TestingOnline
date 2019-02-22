bb472d710e3ffaf62fa60ae71b3629d59ed79785
nhdaihn <39671767+nhdaihn@users.noreply.github.com>
2019-02-22 16:58:18 +0700

    committttt

---
+++ TestingSystem.Data/Repositories/AnswerRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingSystem.Data.Infrastructure;
+using TestingSystem.Models;
+
+namespace TestingSystem.Data.Repositories
+{
+    public interface IAnswerRepository : IRepository<Answer>
+    {
+
+    }
+    public class AnswerRepository : RepositoryBase<Answer>, IAnswerRepository
+    {
+        public AnswerRepository(IDbFactory dbFactory):base(dbFactory)
+        {
+            
+        }
+    }
+}
---
+++ TestingSystem.Sevice/AnswerService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingSystem.Data.Infrastructure;
+using TestingSystem.Data.Repositories;
+using TestingSystem.Models;
+
+namespace TestingSystem.Sevice
+{
+    public interface IAnswerService
+    {
+        void AddAnswer(Answer answer);
+        void UpdateAnswer(Answer answer);
+        void DeleteAnswer(Answer answer);
+    }
+    public class AnswerService : IAnswerService
+    {
+        private readonly IAnswerRepository answerRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public AnswerService(IAnswerRepository answerRepository, IUnitOfWork unitOfWork)
+        {
+            this.answerRepository = answerRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void AddAnswer(Answer answer)
+        {
+             answerRepository.Add(answer);
+        }
+
+        public void DeleteAnswer(Answer answer)
+        {
+            answerRepository.Delete(answer);
+        }
+
+        public void UpdateAnswer(Answer answer)
+        {
+           answerRepository.Update(answer);
+        }
+    }
+}
